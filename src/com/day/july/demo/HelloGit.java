6ac19cc4924a07e253561b9412f1e64e52b1edce
Rina <cfl19893@live.com>
2018-07-09 23:53:37 +0800

    测试git连接

---
+++ src/com/day/july/demo/HelloGit.java
@@ -0,0 +1,9 @@
+package com.day.july.demo;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!"); //Êä³ö
+		System.out.println("ÄãºÃ°¡£¡£¡£¡");
+	}
+
+}
