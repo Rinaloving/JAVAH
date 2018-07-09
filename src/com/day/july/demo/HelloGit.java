ddca63337f63c5689aa08ee3c865c98d53e11039
Rina <cfl19893@live.com>
2018-07-10 00:04:41 +0800

    更新

--- src/com/day/july/demo/HelloGit.java
+++ src/com/day/july/demo/HelloGit.java
@@ -4,6 +4,29 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!"); //输出
 		System.out.println("你好啊！！！");
+		
+		User a = new User();
+		a.setName("小新");
+		System.out.println(a.getName());
+	}
+	
+	
+
+}
+
+class User
+{
+	private String name;
+	public void setName(String name) 
+	{
+		this.name = name;
+		
+	}
+	
+	public String getName() 
+	{
+		
+		return name;
 	}
 
 }
